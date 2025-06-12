e4984cbd18d6b9ee20f3d1d6f986dc8ae8d079ab
kingpipar <masfaremmm@gmail.com>
2025-06-12 08:46:14 +0700

    revisi jadwal kuliah cont dan modelnya

--- src/controller/JadwalKuliahController.java
+++ src/controller/JadwalKuliahController.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
-/**
- *
- * @author user
- */
+import java.util.List;
+import model.JadwalKuliah;
+import model.JadwalKuliahDAO;
+
 public class JadwalKuliahController {
-    
+    private JadwalKuliahDAO jadwalDAO;
+
+    public JadwalKuliahController() {
+        jadwalDAO = new JadwalKuliahDAO();
+    }
+
+    /**
+     * Mengambil semua data jadwal kuliah berdasarkan semester.
+     * @param semester semester yang sedang ditempuh mahasiswa
+     * @return daftar jadwal kuliah
+     */
+    public List<JadwalKuliah> getJadwalBySemester(int semester) {
+        return jadwalDAO.getAllJadwalBySemester(semester);
+    }
 }
